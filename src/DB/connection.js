f4885639ad76250bea3d3be8bf82d962bb93c785
Salome Loor <mariasalomeloorvelez@gmail.com>
2025-11-28 13:39:06 -0500

    Correccion en connection.js

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -5,12 +5,13 @@ import {
   DB_HOST,
   DB_DATABASE,
   DB_PORT,
+  DB_CONNECTION,
 } from "../Config/config.js";
 
 export const sequelize = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
   host: DB_HOST,
   port: DB_PORT,
-  dialect: "postgres",
+  dialect: DB_CONNECTION,
   dialectOptions: {
     ssl: {
       require: true,
