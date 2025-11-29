3297b8eb025e1227c747af23f372b0b32881b847
Salome Loor <mariasalomeloorvelez@gmail.com>
2025-11-28 19:28:39 -0500

    Actaulizacion de cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(morgan('dev')); // Middleware para mostrar las peticiones en consola
 app.use(cors({
     origin: [
         'http://localhost:8100',
-        process.env.FRONTEND_URL
+        'https://myblog-front-end.onrender.com/'
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
